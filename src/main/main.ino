498bb71b13a59aa43aa813569fedb7992be8313c
Jaromir <jaromir.lagutin@gmail.com>
2023-09-29 14:02:32 +0200

    Single cube

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,16 +1,26 @@
 #include <LiquidCrystal.h>
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 9, 8, 7, 6);
+
+int cube = 0;
+
 void setup() {
   // set up the LCD's number of columns and rows
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("hello, world!");
+    lcd.print("Press the button");
+    lcd.setCursor(0,1);
+    lcd.print("for roll");
+    lcd.setCursor(1,0);
+
+ pinMode(13, OUTPUT);
 }
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note line 1 is the second row, since counting begins with 0)
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset
-  lcd.print(millis() / 1000);
+  if(digitalRead(13) == HIGH){
+    cube = random(1,7);
+    lcd.clear();
+    lcd.print("Score:");
+    lcd.print(     cube);
+    delay(750);
+  }
 }
