c72f1e943f59eb695e88603d821b43d80db32d37
Jaromir <jaromir.lagutin@gmail.com>
2023-09-30 14:28:47 +0200

    Usinge GyverButton

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,26 +1,44 @@
 #include <LiquidCrystal.h>
+#include <GyverButton.h>
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 9, 8, 7, 6);
+GButton butt1(13);
+boolean isPress = false;
 
 int cube = 0;
 
 void setup() {
-  // set up the LCD's number of columns and rows
+  buttonSetup();
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-    lcd.print("Press the button");
-    lcd.setCursor(0,1);
-    lcd.print("for roll");
-    lcd.setCursor(1,0);
+  lcd.clear();
 
- pinMode(13, OUTPUT);
+  lcd.print("Press the button");
+  lcd.setCursor(0, 1);
+  lcd.print("for roll");
+  lcd.setCursor(1, 0);
 }
+
+void buttonSetup() {
+  Serial.begin(9600);
+  butt1.setTickMode(AUTO);
+  butt1.setDebounce(50);
+  butt1.setTimeout(300);
+  butt1.setClickTimeout(600);
+  butt1.setType(HIGH_PULL);
+  butt1.setDirection(NORM_OPEN);
+}
+
 void loop() {
-  if(digitalRead(13) == HIGH){
-    cube = random(1,7);
+  if (butt1.isHold() && isPress == false) {
     lcd.clear();
+    lcd.print("Rolling...");
+    isPress = true;
+  }
+  if (butt1.isRelease()) {
+    isPress = false;
+    lcd.clear();
+    cube = random(1, 7);
     lcd.print("Score:");
-    lcd.print(     cube);
-    delay(750);
+    lcd.print(cube);
   }
 }
